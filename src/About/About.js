c0225e7544ee6e37a94cc7d228266cb5e54dffc9
Liam McMahon <mcmahonliam837@gmail.com>
2020-01-03 15:56:00 -0500

    added resume link

--- src/About/About.js
+++ src/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
       <div>
         <h1 className="title">&#x25A0;&thinsp;&thinsp;Hi there, I’m Liam!</h1>
         <p>
-          A graduate of Seneca Colleges Computer Engineering Technology program, Skills Ontario Competition Medalist, current York University Computer Science student and passionate computer enthusiast. There is nothing that I love more than a challange which pushes the limits of my knowledge. This passion is what drove me to represent Seneca at Skills Ontario, to join a student lead research project at seneca, and continue to challange myself now at York.
+          A graduate of Seneca Colleges Computer Engineering Technology program, Skills Ontario Competition Medalist, current York University Computer Science student and passionate computer enthusiast. There is nothing that I love more than a challange which pushes the limits of my knowledge. This passion is what drove me to represent Seneca at Skills Ontario, to join a student lead research project at seneca, and continue to challange myself now at York. If you are interested, here is my <a href='Resume.pdf'  target="_blank" rel="noopener noreferrer"><span className='highlight'>resume</span></a>.
         </p>
       </div>
 
